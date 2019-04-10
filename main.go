bf649ff66636c25dfa243e043da547e7eacd465d
Tornike Razmadze <torniker@gmail.com>
2019-04-10 16:42:01 +0400

    removed unneccesery code

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"github.com/torniker/goapp/app"
-	"github.com/torniker/goapp/app/logger"
-	"github.com/torniker/goapp/should"
 	"github.com/torniker/goapp/web"
 )
 
@@ -13,22 +11,4 @@ func main() {
 		panic(err)
 	}
 	a.Start(":8989")
-}
-
-func home(c *app.Ctx, nextRoute string) error {
-	logger.Infof("nextRoute: %v\n", nextRoute)
-	c.Next(api)
-	return nil
-}
-
-func api(c *app.Ctx, nextRoute string) error {
-	logger.Infof("nextRoute: %v\n", nextRoute)
-	should.Method(c, should.POST)
-	c.Next(auth)
-	return nil
-}
-
-func auth(c *app.Ctx, nextRoute string) error {
-	logger.Infof("nextRoute: %v\n", nextRoute)
-	return nil
 }

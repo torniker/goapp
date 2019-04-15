25b04c97a06777fd36afe7d355d1cc02d53e2f1f
Tornike Razmadze <torniker@gmail.com>
2019-04-15 21:56:04 +0400

    more updates for request and app

--- request/request.go
+++ request/request.go
@@ -1,28 +1,114 @@
 package request
 
 import (
-	"io"
+	"fmt"
+	"net/url"
+	"strings"
 )
 
-// list of request types
+// list of request actions
 const (
-	TypeHTTP int = 1 + iota
-	TypeCLI
-)
-
-// list of request methods
-const (
-	GET int = 1 << iota
-	POST
-	PUT
+	CREATE Action = 1 + iota
+	READ
+	UPDATE
 	DELETE
 )
 
+// Action type for describing request action
+type Action int
+
 // Request defines methods for request object
 type Request interface {
-	Type() int
-	MethodCode() int
-	Input() io.ReadCloser
-	Path() string
-	Query() map[string][]string
+	Action() Action
+	Path() *Path
+	Bind(v interface{}) error
+	SetFlag(key, value string)
+	Flags() map[string][]string
+}
+
+// Path defines structure for command path
+type Path struct {
+	url      *url.URL
+	segments []string
+	index    int
+}
+
+// NewAction returns validated action
+func NewAction(val int) (*Action, error) {
+	a := Action(val)
+	if !a.IsValid() {
+		return nil, fmt.Errorf("invalid value of action")
+	}
+	return &a, nil
+}
+
+// IsValid checks if action has a valid value
+func (a *Action) IsValid() bool {
+	if a == nil {
+		return false
+	}
+	if *a > 0 && *a < 5 {
+		return true
+	}
+	return false
+}
+
+// NewActionFromString returns action
+func NewActionFromString(a string) Action {
+	switch a {
+	case "CREATE":
+		return CREATE
+	case "READ":
+		return READ
+	case "UPDATE":
+		return UPDATE
+	case "DELETE":
+		return DELETE
+	}
+	return Action(0)
+}
+
+// string returns string name of action
+func (a Action) String() string {
+	switch a {
+	case CREATE:
+		return "CREATE"
+	case READ:
+		return "READ"
+	case UPDATE:
+		return "UPDATE"
+	case DELETE:
+		return "DELETE"
+	}
+	return ""
+}
+
+// NewPath creates path object from url
+func NewPath(url *url.URL) *Path {
+	return &Path{
+		url:      url,
+		segments: strings.Split(url.Path, "/"),
+		index:    0,
+	}
+}
+
+// Next returns next segment of the path
+func (p *Path) Next() string {
+	if len(p.segments) < p.index+2 {
+		return ""
+	}
+	return p.segments[p.index+1]
+}
+
+// Current returns current segment of the path
+func (p *Path) Current() string {
+	if len(p.segments) < p.index+1 {
+		return ""
+	}
+	return p.segments[p.index]
+}
+
+// Increment increases path index by 1
+func (p *Path) Increment() {
+	p.index++
 }
